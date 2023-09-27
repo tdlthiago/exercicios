54970c3cdc23ee61e882cddec03b83aca63abf11
THIAGO DUARTE DE LIMA <thiago.dlima2@senacsp.edu.br>
2023-09-27 16:32:52 -0300

    add exercicios da javascript

---
+++ ex1.js
@@ -0,0 +1,42 @@
+/*let n1 = prompt("digite um numero: ")
+let n2 = prompt("digite outro numero: ")
+
+let resultado = parseInt(n1) + parseInt(n2)
+
+alert(resultado)       1 EXERCICIO*/
+
+
+
+/*let base  = prompt( "digite a base ")
+let altura = prompt( "digite a altura ")
+let resposta= (parseInt(base)*parseInt(altura))/2
+
+alert ("o resultado é"+resposta  )   EXERCICIO 2      */
+
+
+/*    3 EXERCICIO
+
+let n1 = prompt("digite sua nota:")
+let n2 = prompt("digite sua nota: ")
+let n3 = prompt("digite sua nota: ")
+
+let resultado = (parseFloat(n1)+parseFloat(n2)+parseFloat(n3))/3
+
+alert ("sua media é"+Math.round(resultado ))*/
+
+/*4 EXERCICIO
+
+
+let matricula = prompt("digite sua matricula")
+let horas = prompt("quantas horas trabalhada")
+let baseSal = prompt("valor por horas de trabalho")
+
+let salario = (parseInt(horas)*parseInt(baseSal))
+
+alert ("seu salario esse mes é"+salario)*/
+
+
+let distancia = prompt("qual distancia percorrida")
+let combustivel = prompt("litragem")
+let final = (parseFloat(distancia)/parseFloat(combustivel))
+alert resultado ("distncia percorrida é +final")
---
+++ script.js
@@ -0,0 +1,13 @@
+ /*let nome = "thais" //string
+let idade = 33 //interiro
+let sexo ="feminino" //string
+let altura = 1.75 //double
+let temgato = false //booleano */
+
+//alert("oi meu nome é "+nome+" e eu tenho "+idade+" anos")
+//alert(`oi meu nome é ${nome} e eu tenho ${idade} anos`)
+
+//console.log("oi meu nome é ${nome} e eu tenho $[idade] anos")
+
+let nome = prompt("ola, qual seu nome?")
+alert("seja bem vindo ")
